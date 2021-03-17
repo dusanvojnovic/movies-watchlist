bb0f474b04a93a7e1ab8d763fe0461f9172c9d1c
Your Name <you@example.com>
2021-03-17 12:10:47 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,158 @@
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_bootstrap import Bootstrap
+from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
+from werkzeug.security import generate_password_hash, check_password_hash
+import requests
+from forms import FindMovieForm, RegisterForm, LoginForm
+
+MOVIE_DB_API_KEY = "3dee8a140ebfcf57cbbd5b3e745e6ad0"
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap(app)
+
+##CREATE DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    movies = relationship("Movie", back_populates="owner")
+db.create_all()
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey("users.id"))
+    owner = relationship("User", back_populates = "movies")
+    title = db.Column(db.String(250), unique=False, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(300), nullable=False)
+db.create_all()
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route("/")
+def home():
+    all_movies = []
+    if current_user.is_authenticated:
+        all_movies = Movie.query.filter_by(owner=current_user).all()
+    return render_template("index.html", movies=all_movies, user=current_user)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+
+        user = User.query.filter_by(email=email).first()
+        if not user:
+            flash("That email doesn't exist")
+            return redirect(url_for('login'))
+        elif not check_password_hash(user.password, password):
+            flash("Password incorrect, please try again")
+            return redirect(url_for('login'))  
+        else:
+            login_user(user)
+            return redirect(url_for('home'))
+    
+    return render_template('login.html',form=form, current_user=current_user)
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        # checking if user with entered email already exist
+        if User.query.filter_by(email=form.email.data).first():
+            flash("You've already signed up with that email, log in instead or try with different email")
+            return redirect(url_for("register"))
+        if User.query.filter_by(username=form.username.data).first():
+            flash("You're already registered with this username.")
+            return redirect(url_for("register"))
+        hash_and_salted_password = generate_password_hash(
+            form.password.data,
+            method = "pbkdf2:sha256",
+            salt_length = 5
+        )
+
+        new_user = User(
+            email = form.email.data,
+            username = form.username.data,
+            password = hash_and_salted_password
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        login_user(new_user)
+        return redirect(url_for('home'))
+    
+    return render_template('register.html', form=form, current_user=current_user)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+    if form.validate_on_submit():
+        movie_title = form.title.data
+
+        response = requests.get(MOVIE_DB_SEARCH_URL, params={"api_key": MOVIE_DB_API_KEY, "query": movie_title})
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+    return render_template("add.html", form=form)
+
+
+@app.route("/find")
+def find_movie():
+    movie_api_id = request.args.get("id")
+    all_movies = []
+    if movie_api_id:
+        movie_api_url = f"{MOVIE_DB_INFO_URL}/{movie_api_id}"
+        response = requests.get(movie_api_url, params={"api_key": MOVIE_DB_API_KEY, "language": "en-US"})
+        data = response.json()
+        if current_user.is_authenticated:
+            new_movie = Movie(
+                owner = current_user,
+                title=data["title"],
+                year=data["release_date"].split("-")[0],
+                img_url=f"{MOVIE_DB_IMAGE_URL}{data['poster_path']}",
+                description=data["overview"]
+            )
+            db.session.add(new_movie)
+            db.session.commit()
+            all_movies = Movie.query.filter_by(owner=current_user).all()
+
+    return redirect(url_for("home"))
+
+
+@app.route("/delete")
+def delete_movie():
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    db.session.delete(movie)
+    db.session.commit()
+    return redirect(url_for("home"))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Movies Watchlist
+
+A site, built with Flask, where registered users can make list of movies that they want to watch. Data is used from https://www.themoviedb.org/
